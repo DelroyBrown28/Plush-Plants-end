0c9d29ec0fbe8ff848608835e2561622eecca413
Delroy <Delroy_jb@hotmail.co.uk>
2020-01-18 18:15:50 +0000

    Store page

---
+++ store.js
@@ -0,0 +1,17 @@
+function newFunction () {
+    document.getElementById('submit-form-btn').innerHTML = "Sent!";
+}
+
+function secondFunction () {
+    var x = document.getElementsByClassName('form-signup-btn');
+    x[0].innerHTML = "Thanks!";
+}
+
+function thirdFunction () {
+    document.getElementById('signup-form-btn-btmleft').innerHTML = "Thanks!";
+}
+
+function basketColor() {
+    document.getElementById("shop-basket").style.color = "yellow";
+  }
+
